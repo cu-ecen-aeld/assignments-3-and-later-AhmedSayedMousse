47e4d67a02dbd73e9e7272112adef16a866c07a4
ahmedsayedmousse <ahmedsayedmousse@gmail.com>
2023-01-12 17:11:08 +0200

    changing the logic that checks for OUTDIR

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -5,7 +5,7 @@
 set -e
 set -u
 
-OUTDIR=${OUTDIR:=/tmp/aeld}
+OUTDIR=${1:-/tmp/aeld}
 KERNEL_REPO=git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
 KERNEL_VERSION=v5.1.10
 BUSYBOX_VERSION=1_33_1
