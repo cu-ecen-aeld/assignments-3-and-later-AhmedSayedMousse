7c6d42834000b8834eb0d17b4c933fe5e2c3f0d2
Ahmed Sayed Mousse <ahmedsayedmousse@gmail.com>
2023-01-29 17:36:51 +0200

    using thread model to accept connections

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -11,6 +11,7 @@
 #include <stdbool.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <pthread.h>
 
 //==============
 // 	MACROS
@@ -28,6 +29,65 @@ int sockfd;
 //====================================
 // 	HELPER FUNCTIONS
 //=====================================
+void *accepting_thread_function(void * accepted_fd){
+	int new_fd = *((int *)accepted_fd);
+	ssize_t no_bytes; // signed
+	char* packetBuffer;
+	FILE* fp;
+	
+	if ((fp = fopen(DATAFILE_PATH, "a+")) == NULL)
+	{
+		perror("OPEN");
+		syslog(LOG_ERR, "OPEN");
+		return (void *)(EXIT_FAILURE);
+	}
+
+	packetBuffer = malloc(BUFFER_SIZE+1);
+	char *line=NULL;
+	size_t len = 0;
+	do
+	{
+		syslog(LOG_INFO, "Entered acceptloop");
+		if ((no_bytes = recv(new_fd, packetBuffer, BUFFER_SIZE, 0)) == -1)
+		{
+			perror("Recieve");
+			syslog(LOG_ERR, "Receive error");
+			return (void *)(EXIT_FAILURE);
+		}
+		packetBuffer[no_bytes] = '\0';
+		if (fprintf(fp, "%s", packetBuffer) <0)
+		{
+			perror("printing to file");
+			syslog(LOG_ERR, "printing to file");
+			return (void *)(EXIT_FAILURE);
+		}
+		if (index(packetBuffer, '\n') != NULL)
+		{
+			// set the start to the begining of the file
+			rewind(fp);
+			// read line by line and send on the socket
+			while((no_bytes = getline(&line, &len, fp)) != -1)
+			{
+				if (send(new_fd, line, no_bytes, 0) == -1)
+				{
+					perror("Failed to send");
+					syslog(LOG_ERR, "Send");
+					return (void *)(EXIT_FAILURE);
+				}
+				syslog(LOG_INFO, "Sent %ld", no_bytes);
+			}
+		}
+		
+	}while(no_bytes >0);
+	syslog(LOG_INFO, "Exited acceptloop");
+	free(packetBuffer);
+	fclose(fp);
+	close(new_fd);
+	return (void *)(EXIT_SUCCESS);
+
+	
+}
+
 void  clean_close(void)
 {
 	syslog(LOG_ERR, "Closing");
@@ -66,11 +126,11 @@ void *get_in_addr(struct sockaddr *sa)
 //==============
 int main(int argc, char** argv)
 {
-	int new_fd, rv, yes=1;
-	char *packetBuffer, s[INET6_ADDRSTRLEN];
-	ssize_t no_bytes;  // this one is signed
+	int  new_fd, rv, yes=1;
+	char s[INET6_ADDRSTRLEN];
 	socklen_t sin_size; // size of addr string
-	FILE *fp;
+	pthread_t thread;
+	void *retval;
 	struct sigaction siga;
 	struct addrinfo hints, *res;
 	struct sockaddr_storage their_addr;
@@ -182,64 +242,40 @@ int main(int argc, char** argv)
 			close(new_fd);
 			clean_close();
 		}
-		// get the address string
+		// Print the ip of the accepted connection
 		if (inet_ntop(their_addr.ss_family,
-			  get_in_addr(their_addrPtr), s, sizeof s) == NULL)
+		  get_in_addr(their_addrPtr), s, sizeof s) == NULL)
 		{
 			syslog(LOG_ERR, "inet_ntop");
 			exit(EXIT_FAILURE);
 		}
 		syslog(LOG_DEBUG, "Accepted connection from %s", s);
-
-		if ((fp = fopen(DATAFILE_PATH, "a+")) == NULL)
+		// create a thread 
+		if (pthread_create(&thread, NULL, accepting_thread_function, (void *)(&new_fd)) != 0)
 		{
-			perror("OPEN");
-			syslog(LOG_ERR, "OPEN");
-			exit(EXIT_FAILURE);
+			perror("Pthread_Create");
+			syslog(LOG_ERR, "Pthread_Create");
+			close(new_fd);
+			clean_close();
 		}
-
-		packetBuffer = malloc(BUFFER_SIZE+1);
-		char *line=NULL;
-		size_t len = 0;
-		do
+		// join on the thread
+		if (pthread_join(thread, &retval) != 0)
 		{
-			syslog(LOG_INFO, "Entered acceptloop");
-			if ((no_bytes = recv(new_fd, packetBuffer, BUFFER_SIZE, 0)) == -1)
-			{
-				perror("Recieve");
-				syslog(LOG_ERR, "Receive error");
-				exit(EXIT_FAILURE);
-			}
-			packetBuffer[no_bytes] = '\0';
-			if (fprintf(fp, "%s", packetBuffer) <0)
-			{
-				perror("printing to file");
-				syslog(LOG_ERR, "printing to file");
-				exit(EXIT_FAILURE);
-			}
-			if (index(packetBuffer, '\n') != NULL)
-			{
-				// set the start to the begining of the file
-				rewind(fp);
-				// read line by line and send on the socket
-				while((no_bytes = getline(&line, &len, fp)) != -1)
-				{
-					if (send(new_fd, line, no_bytes, 0) == -1)
-					{
-						perror("Failed to send");
-						syslog(LOG_ERR, "Send");
-						exit(EXIT_FAILURE);
-					}
-					syslog(LOG_INFO, "Sent %ld", no_bytes);
-				}
-			}
-			
-		}while(no_bytes >0);
-		syslog(LOG_INFO, "Exited acceptloop");
-		free(packetBuffer);
-		fclose(fp);
-		close(new_fd);
-	  	syslog(LOG_INFO, "Closed connection from %s\n", s);
+			perror("Pthread_join");
+			syslog(LOG_ERR, "Pthread_join");
+			close(new_fd);
+			clean_close();
+		}
+		
+		if ( *((int *)retval) == EXIT_FAILURE )
+		{
+			// Some error happend
+			perror("Inside Thread");
+			syslog(LOG_ERR, "Inside Thread");
+			close(new_fd);
+			clean_close();
+		}
+		syslog(LOG_INFO, "Closed connection from %s\n", s);
 	}
 	syslog(LOG_INFO, "Exited mainloop");
 	shutdown(sockfd, SHUT_RDWR);
