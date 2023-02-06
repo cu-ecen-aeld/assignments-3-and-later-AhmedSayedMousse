ebb358eb1cee32af0f7fea35ab6932dbc915e56f
ahmedsayedmousse <ahmedsayedmousse@gmail.com>
2023-02-06 16:57:18 +0200

    Assignment7-part-1

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -32,6 +32,21 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
     /**
     * TODO: implement per description
     */
+    uint8_t i = buffer->out_offs;
+    size_t offs_left = char_offset;
+    do
+    {
+    	if (buffer->entry[i].size > offs_left)
+    	{
+    		*entry_offset_byte_rtn = offs_left;
+    		return &(buffer->entry[i]);
+    	}else
+    	{
+    		offs_left -= buffer->entry[i].size;
+    		
+    	}
+    	i = (i+1)%AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+    }while(i !=buffer->in_offs && offs_left >=0);
     return NULL;
 }
 
@@ -47,6 +62,12 @@ void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const s
     /**
     * TODO: implement per description
     */
+    buffer->entry[buffer->in_offs] = *add_entry;
+    buffer->in_offs = (buffer->in_offs+1)%AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+    if (buffer->full)
+    	buffer->out_offs = (buffer->out_offs+1)%AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+    if (buffer->in_offs == buffer->out_offs)
+    	buffer->full = true;
 }
 
 /**
